bb16455aebbaaf998090fb9af1c6ed3357d87725
Eldar Eminov <em88el@gmail.com>
2023-12-12 12:29:12 +0300

    Temp Update

--- projects/project_0001/lib/manager.rb
+++ projects/project_0001/lib/manager.rb
@@ -62,7 +62,7 @@ class Manager < Hamster::Harvester
 
     #parse_save_main
     #clear_cache        if !keeper.saved.zero? || !keeper.updated.zero?
-    parse_save_lang    if !keeper.saved.zero? || settings['day_lang_all_scrap'] == Date.current.day
+    parse_save_lang    #if !keeper.saved.zero? || settings['day_lang_all_scrap'] == Date.current.day
     parse_save_desc_dd unless keeper.saved.zero?
     export
     keeper.finish
