0926c21cd0de90a9a97f31df7522412beb54af2f
Eldar Eminov <em88el@gmail.com>
2023-12-27 23:59:46 +0300

    3 Update thе SonyGameAdditional

--- projects/project_0003/models/sony_game_additional.rb
+++ projects/project_0003/models/sony_game_additional.rb
@@ -1,7 +1,7 @@
 require_relative './application_record'
 
 class SonyGameAdditional < ApplicationRecord
-  validates :md5_hash, uniqueness: true
+  validates :data_source_url, uniqueness: true
 
   self.table_name = ENV['BD_TABLE_NAME_ADDITIONAL']
 
