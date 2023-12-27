c3db3f19b4719df81118b8362c7bedd1516d42e2
Eldar Eminov <em88el@gmail.com>
2023-12-27 21:52:17 +0300

    3 Update the Keeper

--- projects/project_0003/lib/keeper.rb
+++ projects/project_0003/lib/keeper.rb
@@ -1,4 +1,8 @@
+require_relative '../models/sony_game_ua_run'
+require_relative '../models/sony_game_additional'
 require_relative '../models/sony_game'
+require_relative '../models/sony_game_category'
+require_relative '../models/sony_game_intro'
 
 class Keeper < Hamster::Keeper
   SOURCE     = 3
--- projects/project_0003/models/sony_game_additional_file.rb
+++
@@ -1,5 +0,0 @@
-require_relative './application_record'
-
-class SonyGameAdditionalFile < ApplicationRecord
-  self.table_name = ENV['BD_TABLE_NAME_ADDITIONAL_FILES']
-end
