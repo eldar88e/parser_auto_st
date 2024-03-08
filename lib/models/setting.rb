c185d595a3faad046dc773fb50ec3b1d6174eea8
Eldar Eminov <em88el@gmail.com>
2024-03-09 00:22:46 +0300

    Fix Settings table

--- lib/models/setting.rb
+++ lib/models/setting.rb
@@ -1,7 +1,13 @@
-require_relative '../models/application_record'
+class Setting < ActiveRecord::Base
+  establish_connection(adapter: ENV.fetch('ADAPTER') { 'mysql2' },
+                       host: ENV.fetch('HOST') { 'localhost' },
+                       database: ENV.fetch('DATABASE'),
+                       username: ENV.fetch('USERNAME'),
+                       password: ENV.fetch('PASSWORD'))
 
-class Setting < ApplicationRecord
   self.table_name = ENV['BD_TABLE_NAME_SETTING']
+  self.inheritance_column = :_type_disabled
+
   #ActiveRecord::Base.logger = Logger.new($stdout)
   #self.logger = Logger.new(STDOUT)
 end
