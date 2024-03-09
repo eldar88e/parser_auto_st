580f337737a2b11a138c50dac49feb834016d823
Eldar Eminov <em88el@gmail.com>
2024-03-09 17:49:01 +0300

    Fix SonyGameAdditional

--- projects/project_0002/models/sony_game_additional.rb
+++ projects/project_0002/models/sony_game_additional.rb
@@ -1,9 +1,6 @@
 require_relative './application_record'
 
 class SonyGameAdditional < ApplicationRecord
-  validates :data_source_url, uniqueness: true
 
-  self.table_name = ENV['BD_TABLE_NAME_ADDITIONAL']
-
-  belongs_to :sony_game
+  belongs_to :sony_game, foreign_key: id
 end
