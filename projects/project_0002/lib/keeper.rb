348240f410d5094e96296999dd47024f3df402c3
Eldar Eminov <em88el@gmail.com>
2024-03-09 17:35:01 +0300

    Temp update with pry

--- projects/project_0002/lib/keeper.rb
+++ projects/project_0002/lib/keeper.rb
@@ -59,8 +59,9 @@ class Keeper < Hamster::Keeper
       model.sony_game.update(data) && @count[:updated_desc] += 1
     end
   rescue ActiveRecord::StatementInvalid => e
-    binding.pry
     Hamster.logger.error "ID: #{id} | #{e.message}"
+  rescue
+    binding.pry
   end
 
   def get_ps_ids
