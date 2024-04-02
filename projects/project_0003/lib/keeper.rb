8e47771f9e8583f3968b854cbe5a6d2f070b2e3e
Eldar Eminov <em88el@gmail.com>
2024-04-02 16:42:30 +0300

    3 Temp update

--- projects/project_0003/lib/keeper.rb
+++ projects/project_0003/lib/keeper.rb
@@ -153,7 +153,7 @@ class Keeper < Hamster::Keeper
     #@count[:skipped] += 1 unless check_md5_hash
 
     binding.pry
-    data = { menuindex: @count[:menu_id_count], editedon: Time.current.to_i, editedby: settings['user_id'] content: game[:main][:content] }
+    data = { menuindex: @count[:menu_id_count], editedon: Time.current.to_i, editedby: settings['user_id'], content: game[:main][:content] }
     sony_game.update(data) && @count[:updated_menu_id] += 1 #if @count[:menu_id_count] != sony_game[:menuindex]
   end
 
